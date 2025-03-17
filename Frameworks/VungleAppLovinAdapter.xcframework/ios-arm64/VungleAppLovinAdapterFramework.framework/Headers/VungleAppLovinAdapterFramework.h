2856e23c8a0548f81e9fb4a5a53d8634feb2db94
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:13:07 +0200

    First commit

---
+++ Frameworks/VungleAppLovinAdapter.xcframework/ios-arm64/VungleAppLovinAdapterFramework.framework/Headers/VungleAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  VungleAppLovinAdapterFramework.h
+//  VungleAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 10.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VungleAppLovinAdapterFramework.
+FOUNDATION_EXPORT double VungleAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for VungleAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char VungleAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VungleAppLovinAdapterFramework/PublicHeader.h>
+
+
